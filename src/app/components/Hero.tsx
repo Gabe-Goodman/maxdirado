71f9f4c6dcd35d0e83f72d895589fdb8cda4a0a4
Gabe-Goodman <goodman.g@husky.neu.edu>
2024-11-08 18:48:47 -0500

    Hero component updated

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,17 +1,18 @@
 // components/Hero.tsx
 "use client";
+import styles from "./Hero.module.css";
 import Image from "next/image";
 import MediaQuery from "react-responsive";
 
 export function Hero() {
 	return (
-		<section className="hero">
+		<section className={styles.hero}>
 			<MediaQuery query="(max-device-width: 767px)">
 				<Image
 					src="/images/hero.jpg"
 					alt="Max DiRado"
-					width={100}
-					height={0}
+					width={800} // Adjust width based on your image requirements
+					height={600} // Adjust height based on your image requirements
 					sizes="100vw"
 					style={{
 						width: "100%",
@@ -24,8 +25,8 @@ export function Hero() {
 				<Image
 					src="/images/heroMobileSmall.jpg"
 					alt="Max DiRado"
-					width={50}
-					height={0}
+					width={1920} // Adjust width based on your image requirements
+					height={1080} // Adjust height based on your image requirements
 					sizes="100vw"
 					style={{
 						width: "100%",
@@ -34,15 +35,6 @@ export function Hero() {
 					priority
 				/>
 			</MediaQuery>
-			<style jsx>{`
-				.hero {
-					display: flex;
-					justify-content: center;
-					align-items: center;
-					width: 100%;
-					background-color: #f9f9f9;
-				}
-			`}</style>
 		</section>
 	);
 }
