75a72f226c2d29e9a81e5afe6652d531af843f65
Gabe-Goodman <goodman.g@husky.neu.edu>
2024-11-14 15:32:03 -0500

    Footer links and icons updated with hover styles

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,13 @@
 // components/Footer.tsx
-"use client";
 import Link from "next/link";
 import { FaInstagram, FaTiktok, FaYoutube, FaPatreon } from "react-icons/fa6";
+import styles from "./Footer.module.css";
 
 export function Footer() {
 	return (
-		<footer className="footer">
-			<div className="footer-content-left">
-				<div className="social-icons">
+		<footer className={styles.footer}>
+			<div className={styles.footerContentLeft}>
+				<div className={styles.socialIcons}>
 					<Link
 						target="_blank"
 						href="https://www.instagram.com/diradomax/"
@@ -38,8 +38,8 @@ export function Footer() {
 					</Link>
 				</div>
 			</div>
-			<div className="footer-content-right">
-				<div className="footer-links">
+			<div className={styles.footerContentRight}>
+				<div className={styles.footerLinks}>
 					<ul>
 						<li>
 							<Link href="/">Home</Link>
@@ -59,20 +59,10 @@ export function Footer() {
 								Video
 							</Link>
 						</li>
-
-						{/* <li>
-							<Link href="/tour">Tour</Link>
-						</li>
-						<li>
-							<Link href="/merch">Merch</Link>
-						</li>
-						<li>
-							<Link href="/contact">Contact</Link>
-						</li> */}
 					</ul>
 				</div>
 			</div>
-			<div className="copyright">
+			<div className={styles.copyright}>
 				<p>
 					Copyright Max DiRado.{" "}
 					<a href="https://github.com/Gabe-Goodman?tab=repositories">
@@ -80,67 +70,6 @@ export function Footer() {
 					</a>
 				</p>
 			</div>
-			<style jsx>{`
-				.footer {
-					padding: 20px;
-					background-color: white;
-					border-top: 1px solid #eaeaea;
-				}
-				.footer-content-left {
-					display: flex;
-					flex-direction: column;
-					align-items: flex-end; /* Align everything to the left */
-				}
-				.footer-content-right {
-					display: flex;
-					flex-direction: column;
-					align-items: flex-start; /* Align everything to the left */
-				}
-				.social-icons {
-					color: black;
-					display: flex;
-					gap: 20px;
-					margin-bottom: -30px; /* Add space between icons and links */
-				}
-				.footer-links {
-					display: flex;
-					flex-direction: column;
-				}
-				.footer-links h4 {
-					margin-bottom: 10px;
-					font-weight: bold;
-					color: black;
-				}
-				.footer-links ul {
-					list-style: none;
-					padding: 0;
-					margin: 0;
-					color: black;
-				}
-				.footer-links li {
-					margin-bottom: 8px;
-				}
-				.footer-links a {
-					text-decoration: none;
-					color: black;
-					font-size: 16px;
-				}
-				.footer-links a:hover {
-					text-decoration: underline;
-				}
-				.copyright {
-					margin-top: 8px;
-					color: #60676a;
-					font-size: 12px;
-				}
-				.copyright a {
-					text-decoration: underline;
-				}
-				p {
-					align-items: center;
-					text-align: center;
-				}
-			`}</style>
 		</footer>
 	);
 }
